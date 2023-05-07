27b7b3a46db89106afe64871a65e125653353194
Fernando Mors <fernandomors80@gmail.com>
2023-05-06 21:26:02 -0300

    first commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App eventos - Lifeados
+//
+//  Created by Poliana Almeida on 06/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
