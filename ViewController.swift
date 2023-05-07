740bf43a051c89c235c0e7092c2673fcc0a4752d
Fernando Mors <fernandomors80@gmail.com>
2023-05-07 00:19:38 -0300

    create tableView

--- ViewController.swift
+++ ViewController.swift
@@ -1,19 +1,47 @@
-//
-//  ViewController.swift
-//  App eventos - Lifeados
-//
-//  Created by Poliana Almeida on 06/05/23.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    lazy var tableView: UITableView = {
+            let tableView = UITableView()
+            tableView.isScrollEnabled = true
+            tableView.separatorStyle = .singleLine
+            tableView.register(UITableViewCell.self, forCellReuseIdentifier:  "Cell")
+            tableView.verticalScrollIndicatorInsets = .zero
+        
+            return tableView
+        }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        setupUI()
+        tableView.dataSource = self
     }
+    
+    private func setupUI() {
+            view.backgroundColor = .white
 
+            view.addSubview(tableView)
+            tableView.translatesAutoresizingMaskIntoConstraints = false
 
+            NSLayoutConstraint.activate([
+                tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+                tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+                tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+                tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            ])
+        }
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(  _ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 30
+    }
+
+    func tableView(  _ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        cell.selectionStyle = .none
+        return cell
+    }
+}
